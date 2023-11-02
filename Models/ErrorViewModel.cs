9dd67f4c721ea48513d7277f65a251e19fe8a822
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2023-11-03 12:10:17 +1300

    Analyse and understand codebase

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,8 +1,11 @@
 namespace todo_fullstack_c_.Models;
 
+// defines a new class called ErrorViewModel. The below logic waits for an error to occur before it gets and exposes an ID, thus triggering the View model for an error 
 public class ErrorViewModel
 {
+  // gets and sets a requestId, which is dynamic 
     public string? RequestId { get; set; }
 
+// conditionally show the RequestId if it exists (i.e. if not null or empty)
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
+// This code is the entry point of the ASP.NET Core application 
+
+// Creates a new instance of a web application, using the provided CLI args 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Adds support for controllers with views, to make it compatible with an MVC architecture  
 builder.Services.AddControllersWithViews();
 
+// Builds the app with the specified configuration 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+// If the app is not in a development env, use a specific error page when errors occur, and require an HTTPS connection
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -13,13 +19,17 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+// Sets up routing for the application, so that it knows which controller and action to execute based on the URL req
 app.UseRouting();
 
+// Protect parts of app based on your defined policies 
 app.UseAuthorization();
 
+// Configures the default route for your app - root URL defaults to Index in the Home controller 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
